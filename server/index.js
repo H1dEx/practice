748a0f67611652aa840fb91be8b101542074ec38
Oleg Aleksandrin <olegro@MacBookAir-Oleg.local>
2022-04-10 15:48:03 +0300

    refactore project structure

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express();
-
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-    console.log('Server has been started on port ' + PORT)
-})
