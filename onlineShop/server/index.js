d608c7d2a9c062196731decbfc50118ce038f80c
Oleg Aleksandrin <olegro@MacBookAir-Oleg.local>
2022-04-12 23:40:24 +0300

    create db models

--- onlineShop/server/index.js
+++ onlineShop/server/index.js
@@ -1,9 +1,21 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models');
 
 const PORT = process.env.PORT || 5002;
-const app = express()
+const app = express();
 
-app.listen(PORT, () => {
-  console.log('Server has been started on port ' + PORT)
-})
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log('Server has been started on port ' + PORT)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start();
